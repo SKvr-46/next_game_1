e8a52cd5d87708e8590d6071116596cb45c1ed30
SKvr-46 <koeich4@gmail.com>
2023-02-27 23:12:13 +0900

    skeptikal commit

--- component/memoarea.tsx
+++ component/memoarea.tsx
@@ -3,8 +3,8 @@ import styles from "styles/memoarea.module.scss"
 export const MemoArea = () => {
 
     return(
-        <div className={styles.textarea}>
-            <textarea name="Memo" rows={10} cols={50} defaultValue="メモ欄"/>
+        <div className={styles.textArea}>
+            <textarea name="Memo" rows={10} cols={50} placeholder="メモ欄"/>
         </div>
     )
 }
